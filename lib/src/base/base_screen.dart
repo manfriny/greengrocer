fdf9177c84c031ecba18928cc35e967913cb1274
Manfriny Pereira <manfriny@gmail.com>
2022-11-30 11:30:32 -0300

    Aula19 Finalizada

--- lib/src/base/base_screen.dart
+++ lib/src/base/base_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/home/home_tab.dart';
 
 class BaseScreen extends StatefulWidget {
   const BaseScreen({super.key});
@@ -20,7 +21,7 @@ class _BaseScreenState extends State<BaseScreen> {
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
         children: [
-          Container(color: Colors.red),
+          const HomeTab(),
           Container(color: Colors.yellow),
           Container(color: Colors.blue),
           Container(color: Colors.purple),
